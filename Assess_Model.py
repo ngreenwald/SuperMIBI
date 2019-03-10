ada852440d2c74673d571c3933d25719b3ae15ec
Ubuntu <ubuntu@ip-172-31-4-33.us-east-2.compute.internal>
2019-03-10 04:45:47 +0000

    weird

--- Assess_Model.py
+++ Assess_Model.py
@@ -10,17 +10,23 @@ current_model = models.load_model('my_first_model')
 x_data_test = h5py.File(base_dir + 'x_cropped_test.h5', 'r')
 y_data_test = h5py.File(base_dir + 'y_cropped_test.h5','r') 
 
+chans = np.load(base_dir + 'chan_names.npy')
+keepers = ['dsDNA.tif', 'H3K9Ac.tif']
+keep_idx = np.isin(chans, keepers)
+
 x_test = x_data_test['x_test'][:]
 y_test = y_data_test['y_test'][:]
 
 test_idx = np.arange(5)
-y_hat = current_model.predict(x_test[test_idx, :, :, 5:7])
-xs = x_test[test_idx, :, :, 5:7]
-ys = y_test[test_idx, :, :, 5:7]
+xs = x_test[test_idx, :, :, :]
+xs = xs[:, :, :, keep_idx]
+ys = y_test[test_idx, :, :, :]
+ys = ys[:, :, :, keep_idx]
 
+y_hat = current_model.predict(xs)
 np.save('xs.npy', xs)
 np.save('ys.npy', ys)
 np.save('y_hat.npy', y_hat)
 for i in test_idx:
-    y_hat_metrics = current_model.evaluate(x_test[i:(i + 1), :, :, 5:7], y_test[i:(i + 1), :, :, 5:7])
+    y_hat_metrics = current_model.evaluate(xs[i:(i + 1), :, :, :], ys[i:(i + 1), :, :, :])
     np.savetxt('metrics{}'.format(i), y_hat_metrics)
