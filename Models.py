a410f6605703c3db2fa2d33ff61154288b15f1c5
Ubuntu <ubuntu@ip-172-31-4-33.us-east-2.compute.internal>
2019-03-17 20:25:15 +0000

    resset models to try CARE

--- Models.py
+++ Models.py
@@ -13,16 +13,16 @@ def SuperMIBI_1(input_image):
     x_input = Input(input_image)
 
     # first layer
-    x = Conv2D(filters=128, kernel_size=(9, 9), strides=(1, 1), padding='same', name='conv1',
+    x = Conv2D(filters=128, kernel_size=(11, 11), strides=(1, 1), padding='same', name='conv1',
                kernel_initializer=glorot_uniform(seed=0), kernel_regularizer=regularizers.l2(0.01))(x_input)
     x = BatchNormalization(axis=3, name='bn_1')(x)
     x = Activation('relu', name='relu_1')(x)
 
     # second layer
-    x = Conv2D(filters=64, kernel_size=(20, 20), strides=(1, 1), padding='same', name='conv2',
-               kernel_initializer=glorot_uniform(seed=0), kernel_regularizer=regularizers.l2(0.01))(x)
-    x = BatchNormalization(axis=3, name='bn_2')(x)
-    x = Activation('relu', name='relu_2')(x)
+    #x = Conv2D(filters=64, kernel_size=(20, 20), strides=(1, 1), padding='same', name='conv2',
+    #           kernel_initializer=glorot_uniform(seed=0), kernel_regularizer=regularizers.l2(0.01))(x)
+    #x = BatchNormalization(axis=3, name='bn_2')(x)
+    #x = Activation('relu', name='relu_2')(x)
 
     # third layer: 1x1 convolution
 
@@ -31,8 +31,14 @@ def SuperMIBI_1(input_image):
     x = BatchNormalization(axis=3, name='bn_3')(x)
     x = Activation('relu', name='relu_3')(x)
 
-    # fourth layer: predict
+    # optional large layer
+    #x = Conv2D(filters=5, kernel_size=(20, 20), strides=(1, 1), padding='same', name='conv2',
+    #           kernel_initializer=glorot_uniform(seed=0), kernel_regularizer=regularizers.l2(0.01))(x)
+    #x = BatchNormalization(axis=3, name='bn_2')(x)
+    #x = Activation('relu', name='relu_2')(x)
 
+
+    # fourth layer: predict
     x = Conv2D(filters=input_image[2], kernel_size=(5, 5), strides=(1, 1), padding='same', name='conv4',
                kernel_initializer=glorot_uniform(seed=0), kernel_regularizer=regularizers.l2(0.01))(x)
     x = BatchNormalization(axis=3, name='bn_4')(x)
